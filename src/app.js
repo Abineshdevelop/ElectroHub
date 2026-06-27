0361c6428a2d98ea0ef6df94474b4bc8c8197d84
Abineshdevelop <abineshbabu28@gmail.com>
2026-06-27 18:47:05 +0530

    console

--- src/app.js
+++ src/app.js
@@ -47,6 +47,11 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+    console.log(`Port ${process.env.PORT} handled ${req.method} ${req.url}`);
+    next();
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(checkUserBlocked);
